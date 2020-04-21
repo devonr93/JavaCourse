0cbfbe54cd76d2cb3f24a2fca618ac244af91e58
devonr93 <34263704+devonr93@users.noreply.github.com>
2020-04-21 15:34:46 -0500

    Add files via upload

---
+++ AreaCalculator/src/com/company/Main.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(AreaCalculator.area(12));
+        System.out.println(AreaCalculator.area(1, 1));
+    }
+}
